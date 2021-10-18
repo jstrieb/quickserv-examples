3d21114425b81514adc3dd3c1da67fe01b2c6e16
Logan Snow <4197432+lsnow99@users.noreply.github.com>
2021-10-17 22:50:58 -0400

    Add an example using deno

---
+++ movie-list/datastore.ts
@@ -0,0 +1,59 @@
+import type Movie from "./movie.ts";
+
+const decoder = new TextDecoder("utf-8");
+const encoder = new TextEncoder();
+const STOREFILE = "./store.json";
+
+const writeAll = async(data : Uint8Array) => {
+    const file = await Deno.open(STOREFILE, { write: true, create: true, truncate: true })
+    for(let written = 0; written < data.length;) {
+        written += Deno.writeSync(file.rid, data.subarray(written))
+    }
+    file.close()
+}
+
+const readAll = async() => {
+    try {
+        const data = await Deno.readFileSync(STOREFILE)
+        if (data.length < 2) {
+            return {}
+        }
+        return JSON.parse(decoder.decode(data))
+    } catch (error) {
+        if (error && error.name && error.name === "NotFound") {
+            return {}
+        }
+        throw error
+    }
+}
+
+export const newMovie = async (movie: Movie) => {
+  movie.id = crypto.randomUUID();
+  movie.watched = false;
+  await setMovie(movie);
+};
+
+export const getMovieList = async () : Promise<Movie[]> => {
+    try {
+        const movies = await readAll();
+        return Object.values(movies);
+    } catch (error) {
+        console.log('wat')
+        console.log(error)
+        Deno.exit(0);
+    }
+};
+
+export const setMovie = async (movie: Movie) => {
+  const movies = await readAll();
+  movies[movie.id] = movie;
+  const data = encoder.encode(JSON.stringify(movies));
+  await writeAll(data);
+};
+
+export const deleteMovie = async (movie: Movie) => {
+  const movies = await readAll();
+  delete movies[movie.id];
+  const data = encoder.encode(JSON.stringify(movies));
+  await writeAll(data);
+};
---
+++ movie-list/deleteMovie.ts
@@ -0,0 +1,19 @@
+#!deno run --allow-read=./store.json --allow-write=./store.json
+import type Movie from "./movie.ts";
+import { deleteMovie } from "./datastore.ts";
+
+const buf = new Uint8Array(2048);
+const n = await Deno.stdin.read(buf);
+if (n == null) {
+  Deno.exit(1);
+}
+
+const decoder = new TextDecoder("utf-8");
+const movie: Movie = JSON.parse(decoder.decode(buf.subarray(0, n)));
+
+try {
+    await deleteMovie(movie);
+} catch (error) {
+    console.error(error);
+    Deno.exit(1);
+}
